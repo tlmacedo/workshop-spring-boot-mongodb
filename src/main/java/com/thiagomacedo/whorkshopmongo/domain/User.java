fdc231b58a52f9a0e0ae21c19db123a6aeeec31c
Thiago Macedo <tl.macedo@hotmail.com>
2018-10-25 15:26:43 -0400

    Entity user e REST funcionando

---
+++ src/main/java/com/thiagomacedo/whorkshopmongo/domain/User.java
@@ -0,0 +1,60 @@
+package com.thiagomacedo.whorkshopmongo.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
