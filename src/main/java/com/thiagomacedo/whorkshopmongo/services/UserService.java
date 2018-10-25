1e6ed01e38dc1edc496f61645eea601d638907f0
Thiago Macedo <tl.macedo@hotmail.com>
2018-10-25 16:33:18 -0400

    Obtendo um usuario por id

--- src/main/java/com/thiagomacedo/whorkshopmongo/services/UserService.java
+++ src/main/java/com/thiagomacedo/whorkshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.thiagomacedo.whorkshopmongo.services;
 
 import com.thiagomacedo.whorkshopmongo.domain.User;
 import com.thiagomacedo.whorkshopmongo.repository.UserRepository;
+import com.thiagomacedo.whorkshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
