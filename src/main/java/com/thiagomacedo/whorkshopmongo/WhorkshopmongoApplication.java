8ebf964299f935f97e3cc49efcf973717679babf
Thiago Macedo <tl.macedo@hotmail.com>
2018-10-25 15:09:18 -0400

    Projeto criado

---
+++ src/main/java/com/thiagomacedo/whorkshopmongo/WhorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+package com.thiagomacedo.whorkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshopmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhorkshopmongoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/thiagomacedo/whorkshopmongo/WhorkshopmongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thiagomacedo.whorkshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhorkshopmongoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
